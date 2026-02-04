ae923489d3e1567dc56fcb03ffd6c6f568498159
MultazamMahmud12 <multazammahmud@gmail.com>
2026-02-04 17:36:39 +0600

    frontend

---
+++ frontend/src/routers/router.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import EnhancedDashboard from "../pages/EnhancedDashboard";
+import Transactions from "../pages/Transactions";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "login",
+                element: <Login />
+            },
+            {
+                path: "register",
+                element: <Register />
+            },
+            {
+                path: "dashboard",
+                element: <EnhancedDashboard />
+            }
+
+
+
+        ]
+    },
+]);
+
+
+export default router; //exporting to use on main.jsx 
