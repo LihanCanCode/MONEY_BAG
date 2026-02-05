3ac177801e28d9764cbb38ad377ccb5fe100ee91
MultazamMahmud12 <multazammahmud@gmail.com>
2026-02-05 11:22:01 +0600

    backend2

---
+++ backend/src/users/users.model.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+/*
+User {
+  firebaseUid,
+  name,
+  email
+}
+ */
+
+const userSchema = new mongoose.Schema({
+  firebaseUid: {
+    type: String,
+    required: true,
+
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+const user = mongoose.model('user', userSchema, 'users');
+
+module.exports = user;
